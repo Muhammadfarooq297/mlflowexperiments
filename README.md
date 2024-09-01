d54ee648ddd4fc8d71fe90502b4a02ab2593c1a4
SCM <mf5074111@gmail.com>
2024-09-01 17:38:09 +0500

    Second commit

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ## ML FLow experiements
 
-MLFLOW_TRACKING_URI=https://dagshub.com/krishnaik06/mlflowexperiments.mlflow \
-MLFLOW_TRACKING_USERNAME=krishnaik06 \
-MLFLOW_TRACKING_PASSWORD=7104284f1bb44ece21e0e2adb4e36a250ae3251f \
+MLFLOW_TRACKING_URI=https://dagshub.com/Muhammadfarooq297/mlflowexperiments.mlflow \
+MLFLOW_TRACKING_USERNAME=Muhammadfarooq297 \
+MLFLOW_TRACKING_PASSWORD=b1f18e476385f185381259c60e832d2e369e5f8e \
 python script.py
-
--- app.py
+++ app.py
@@ -18,6 +18,7 @@ import mlflow.sklearn
 
 import logging
 
+
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
@@ -80,8 +81,8 @@ if __name__ == "__main__":
 
         ## For Remote server only(DAGShub)
 
-        remote_server_uri="https://dagshub.com/krishnaik06/mlflowexperiments.mlflow"
-        # mlflow.set_tracking_uri(remote_server_uri)
+        remote_server_uri="https://dagshub.com/Muhammadfarooq297/mlflowexperiments.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
